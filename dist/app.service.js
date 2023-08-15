027d914efa063f23e56bd7a47475a608f416fa92
nimur-hasan <developernaim20@gmail.com>
2023-08-16 01:54:50 +0600

    ⛑️ Clean Unnecessery Code

--- dist/app.service.js
+++ dist/app.service.js
@@ -6,17 +6,14 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.AppModule = void 0;
+exports.AppService = void 0;
 const common_1 = require("@nestjs/common");
-const app_controller_1 = require("./app.controller");
-const app_service_1 = require("./app.service");
-let AppModule = exports.AppModule = class AppModule {
+let AppService = exports.AppService = class AppService {
+    getHello() {
+        return 'Hello World!';
+    }
 };
-exports.AppModule = AppModule = __decorate([
-    (0, common_1.Module)({
-        imports: [],
-        controllers: [app_controller_1.AppController],
-        providers: [app_service_1.AppService],
-    })
-], AppModule);
-//# sourceMappingURL=app.module.js.map
+exports.AppService = AppService = __decorate([
+    (0, common_1.Injectable)()
+], AppService);
+//# sourceMappingURL=app.service.js.map
